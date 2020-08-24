134d687856511c33c82d3aa4d41e6e09d0f35892
goldenbijoy <15130913+goldenbijoy@users.noreply.github.com>
2020-08-24 17:58:56 +0600

    basic structure complete except calculation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+
+var arr = ["qweeeeee eeeeeeeeeeeee eeeeeeeeeeee eeeeeeeee eeeeeeeee eeeeeeeeeeeee  eeeeeeeeeeeeeee eeeeeeeeeeeee eeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeee eeeeeeeeee eeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeee eeeeeeeer eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeereereeeer","asdfsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss","jkl","mnop"];
+var start = false;
+var timer = 0;
+var charTyped = 0;
+
+//interval function to update timer
+function time_count() {
+    timer--;
+    var t = timer+'s';
+    $('.curr_time').text(t);
+    if(timer==0){
+        $("textarea").prop('disabled', true);
+    }
+}
+
 $(document).ready(function(){
    
   // jQuery methods go here...
@@ -8,9 +24,10 @@ $(document).ready(function(){
         var x = 'label[for="'+id+'"]';
         $(x).css("color","blue");
     });
-  // function to select time and paragraph value
+  // function for new interface
     var min,story;
     $("#start").click(function(){
+        
         min = $('.zz:checked').val();
         story = $('select.opt').children('option:selected').val();
         if(!min){
@@ -19,12 +36,52 @@ $(document).ready(function(){
         else{
             $('.para, .time').hide();
             $(this).hide();
+            $()
             $('.box').show();
             $('.words').show();
-            // $('.box textarea').val('');
+            $('.calculations').show();
+            var t = (min*60)+'s';
+            $('.curr_time').text(t);
+            for(var i=0;i<arr[story-1].length;i++){
+                $('.wordcontainer').append('<span>'+arr[story-1][i]+'</span>');
+            }
         }
     });
+
+    $('textarea').on("keydown", function (e) {
+        if (e.which === 8 && !$(e.target).is("")) {
+            e.preventDefault();
+        }
+    });
+
+  // start game when started type in textarea and check each charecter typed
     
+    $('textarea').keypress(function(e) {
+        if(!start){
+          setInterval(time_count,1000);
+          start = true;
+          timer = min*60;
+        }
+        // ++charTyped;
+        var code = e.keyCode || e.which;
+        var res = String.fromCharCode(code);
+        var char = $('.wordcontainer span').eq(charTyped).text();
+        if(code==8){
+            return ;
+        }
+        if(char === res){
+            $('.wordcontainer span').eq(charTyped).addClass('correct');
+        }
+        else{
+            $('.wordcontainer span').eq(charTyped).addClass('wrong');
+        }
+
+        charTyped++;
+        
+
+
+    });
 
 
 });
+
