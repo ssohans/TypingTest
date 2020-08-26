6d22826f52dceda497e6f00144b9079dcb2c27e6
goldenbijoy <15130913+goldenbijoy@users.noreply.github.com>
2020-08-26 08:55:49 +0600

    Added stop button

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ function finish(){
     $('.wpm').show();
     $('.cpm').show();
     $('#home').show();
+    $('#stop').hide();
 }
 
 $(document).ready(function(){
@@ -79,6 +80,10 @@ $(document).ready(function(){
     }
 });
 
+  $('#stop').click(function() {
+      finish();
+  });
+
   $('textarea').on("keydown", function (e) {
     if (e.which === 8 && !$(e.target).is("")) {
         e.preventDefault();
@@ -91,6 +96,7 @@ $(document).ready(function(){
   $('textarea').keypress(function(e) {
 
         if(!start){
+          $('#stop').show();
           interval = setInterval(time_count,1000);
           start = true;
           timer = min*60;
